4c8edc1b8e03ad64665c72bf2d10819b4ab88438
Lillian N <ngo.lilli94@gmail.com>
2018-09-20 16:49:48 -0400

    Active Storage set up

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ApplicationRecord
     has_many :tutorials, :dependent => nullify
+    has_one_attached :icon
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,3 +1,5 @@
 class Tutorial < ApplicationRecord
     belongs_to :topic
+    has_one_attached :icon
+    has_many_attached :figures
 end
