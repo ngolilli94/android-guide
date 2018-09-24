177072a55505fc1e67a42160ec09e7e941a6449f
Lillian N <ngo.lilli94@gmail.com>
2018-09-24 10:41:39 -0400

    Seeded topics data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Topic.create(title: 'Parts of a Phone')
+Topic.create(title:'Gestures')
+Topic.create(title: 'How-To')
+Topic.create(title: 'Commong Symbols')
+Topic.create(title: 'Phone Settings')
+Topic.create(title: 'Index')
